e78628871b09e43d5b4c733272fcb3a713503568
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-25 19:35:03 -0700

    Changing the console.log to alert completed and adding event listener to cat image and given alert message complete

--- about.js
+++ about.js
@@ -1,14 +1,15 @@
-console.log("hello world");
-
-
+console.log('hello world');
 
 function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	console.log('form submit');
-}
+  evt.preventDefault();
 
+  alert('Form submitted successfully.');
+}
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+const catImage = document.querySelector('.cat-image');
+catImage.addEventListener('mouseover', () => {
+  alert('This a beautiful image of cat!');
+});
