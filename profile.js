1983b57c9e96b4d688e8b6c13b9ee30eb6233721
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-25 20:49:05 -0700

    Part 3 to Part 5 is completed

---
+++ profile.js
@@ -0,0 +1,12 @@
+document.querySelector('#color').addEventListener('click', (e) => {
+  e.preventDefault();
+  alert(`My favorite color is "White"`);
+});
+document.querySelector('#place').addEventListener('click', (e) => {
+  e.preventDefault();
+  alert(`My favorite place is "My Home Town"`);
+});
+document.querySelector('#ritual').addEventListener('click', (e) => {
+  e.preventDefault();
+  alert(`My favorite ritual is "I don't know"`);
+});
